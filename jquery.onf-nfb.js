410f97ab415effb87fe96ed6b0d695f588db4667
Nicolas Brassard <nico@nitriques.com>
2012-03-02 10:39:59 -0500

    Finished html + css integration

--- jquery.onf-nfb.js
+++ jquery.onf-nfb.js
@@ -67,16 +67,16 @@
 			opacityHover: 1,
 			links: [
 			     {title: {fr:'Accueil',en:'Home'},		url: null, 
-			      callback: null, preventDefault:true, target: null, cssClass: null, tagName: null
+			      callback: null, preventDefault:true, target: null, cssClass: 'onf-bot-cont onf-bot-border', tagName: null
 			     },
 			     {title: {fr:'À propos',en:'About'},	url: null,
-			      callback: null, preventDefault:true, target: null, cssClass: null, tagName: null
+			      callback: null, preventDefault:true, target: null, cssClass: 'onf-bot-cont onf-bot-border', tagName: null
 			     },
 			     {title: {fr:'Films reliés',en:'Related movies'},url: null,
-			      callback: null, preventDefault:true, target: null, cssClass: null, tagName: null
+			      callback: null, preventDefault:true, target: null, cssClass: 'onf-bot-cont onf-bot-border', tagName: null
 			     },
 			     {title: {fr:'Équipe',en:'Credits'},	url: null, 
-			      callback: null, preventDefault:true, target: null, cssClass: null, tagName: null
+			      callback: null, preventDefault:true, target: null, cssClass: 'onf-bot-cont', tagName: null
 			     }
 			],
 			share: {
@@ -98,22 +98,13 @@
 	
 	/** Private common functions **/
 	function _getValue(o) {
-		if ($.isFunction(o)) {
-			return o.call();
-		}
-		return o;
+		return $.isFunction(o) ? o.call(this) : o;
 	};
 	function _getObjectValue(o) {
-		if ($.isPlainObject(o)) {
-			return o[LG];
-		}
-		return _getValue(o);
+		return $.isPlainObject(o) ? o[LG] : _getValue(o);
 	};
 	function _getDefaultValue(o, d) {
-		if (!!o) {
-			return o;
-		}
-		return d;
+		return !!o ? o : d;
 	};
 	function preventDefault(e) {
 		if (!!e && $.isFunction(e.preventDefault)) {
@@ -152,18 +143,22 @@
 			value = _getValue(opts.opacityHover);
 		}
 		
-		t.stop(false, false).animate({opacity:value}, 250);
+		t.stop(true, false).animate({opacity:value}, 250);
 	};
 	function _hoverIn(e) {
-		_hover.call(this, e);
+		_hover.call(this, e, true);
 	};
 	function _hoverOut(e) {
-		_hover.call(this, e);
+		_hover.call(this, e, false);
 	};
 	function _createLink(l) {
 		var a = $('<a></a>');
 		a.text(_getObjectValue(l.title));
-		a.attr('href', _getObjectValue(l.url));
+		if (!!l.url) {
+			a.attr('href', _getObjectValue(l.url));
+		} else {
+			a.attr('href', "#");
+		}
 		a.data('link', l);
 		if (!!l.target) {
 			a.attr('target', _getValue(l.target));
@@ -235,39 +230,109 @@
 	
 	
 	/* Menu top */
+	function search(e) {
+		
+		return preventDefault(e);
+	};
+	function searchToggle(e, show) {
+		var pnl = $('#onf-top-search-pnl'),
+			lbl = $('#onf-top-search-lbl')
+			
+		// improve logic here !!
+		pnl.fadeTo(400, show ? 1 : 0);
+		lbl.fadeTo(400, show ? 0 : 1);
+		
+		return preventDefault(e);
+	};
+	function searchIn(e) {
+		return searchToggle.call(this, e, true);
+	};
+	function searchOut(e) {
+		return searchToggle.call(this, e, false);
+	};
+	function searchKeyDown(e) {
+		switch (e.which) {
+			case 27 : // escape
+				searchOut.call(this);
+				break;
+			case 13: // enter
+				search();
+				break;
+		}
+		return true;
+	};
+	
 	function menuTop(top_options) {
-		var opts = $.extend(true, opts, top_options),
+		var opts = $.extend(true, opts, top_defaults),
 			target = $(opts.target),
-			opacityEnabled = _getValue(opts.opacity) - _getValue(opts.opacityHover) > 0,
+			opacityEnabled = Math.abs(_getValue(opts.opacity) - _getValue(opts.opacityHover)) > 0,
+			logo = $('<a id="onf-top-logo"></a>'),
+			search_wrap = $('<span id="onf-top-search"></span>'),
+			search_lbl = $('<a href="#" id="onf-top-search-lbl"></span>'),
+			search_pnl = $('<div id="onf-top-search-pnl"></div>'),
+			search_txt = $('<input id="onf-top-search-txt" type="text" />'),
+			search_btn = $('<input id="onf-top-search-btn" type="button" value="" />'),
+			right_wrap = $('<div id="onf-top-right"></span>'),
 			wrap = $('<div id="onf-top-wrap"></div>');
 		
-			
+		// create logo
+		logo.attr('href', ONF_NFB_url);
+		
+		wrap.append(logo);
+		
+		// create menu items
+		for (var i in opts.links) {
+			wrap.append(_createLink(opts.links[i]));
+		}
+		
+		// create search function
+		search_lbl.text(_getObjectValue(opts.search));
+		search_lbl.click(searchIn);
+		
+		search_txt.attr('placeholder', _getObjectValue(opts.search));
+		search_txt.keydown(searchKeyDown);
+		search_btn.click(search);
+		
+		search_pnl.append(search_txt).append(search_btn);
+		
+		search_wrap.append(search_lbl).append(search_pnl);
+		
+		wrap.append(search_wrap);
+		
+		// create right menus items
+		right_wrap.append(_createLink(opts.help));
+		for (var i in opts.translate) {
+			right_wrap.append(_createLink(opts.translate[i]));
+		}
+		wrap.append(right_wrap);
+		
+		// finally add the wrap to our container
+		target.empty().append(wrap);
+		
+		// save options
+		target.data('opts', opts);
+		
 		// handle hover
 		if (opacityEnabled) {
 			target.hover(_hoverIn, _hoverOut);
+			_hoverOut.call(target);
 		}
-			
-		// finally add the wrap to our container
-		target.empty().add(wrap);
 	};
 	
 	
 	/* Menu bottom */
-	
 	function shareToggle(e, isIn) {
 		var ow = 165,
-			w = 65;
+			w = 65,
+			share = $('#onf-bot-share');
 		
 		if (isIn) {
 			w = ow;
 		}
 		
-		share.css({display:'inline-block'});
-		share.stop(false, false).animate({width:w}, ow * 2, function () {
-			if (!isIn) {
-				//share_opts.hide(0);
-			}
-		});
+		share.stop(false, false)
+			 .css({display:'inline-block'})
+			 .animate({width:w}, ow * 2);
 		
 		return preventDefault(e);
 	};
@@ -280,19 +345,59 @@
 	
 	
 	function menuBot(bot_options) {
-		var opts = $.extend(true, opts, bot_options),
+		var opts = $.extend(true, opts, bot_defaults),
 			target = $(opts.target),
-			opacityEnabled = _getValue(opts.opacity) - _getValue(opts.opacityHover) > 0,
-			wrap = $('<div id="onf-bot-wrap"></div>');
+			opacityEnabled = Math.abs(_getValue(opts.opacity) - _getValue(opts.opacityHover)) > 0,
+			wrap = $('<div id="onf-bot-wrap"></div>'),
+			right_wrap = $('<span id="onf-bot-right"></span>'),
+			share_wrap = $('<span id="onf-bot-share"></span>'),
+			share_opts = $('<span id="onf-share-opts"></span>'),
+			vol_btn = $('<a href="#" id="onf-volume"></a>');
+		
+		// create menu item
+		for (var i in opts.links) {
+			wrap.append(_createLink(opts.links[i]));
+		}
+		
+		// create share menu items
+		share_wrap.append($('<span>' + _getObjectValue(opts.share) + '</span>'));
+		for (var i in opts.share.links) {
+			var l = opts.share.links[i], 
+				a = $('<a></a>');
 			
+			a.attr('href', _getValue(l) );
+			a.attr('data-name', i);
+			a.attr('id', 'onf-' + i);
+			a.attr('class', 'onf-social');
+			a.attr('target', '_blank');
+			a.click($.noop);
 			
+			share_opts.append(a);
+		}
+		share_wrap.mouseenter(share_in);
+		target.mouseleave(share_out);
+		share_wrap.append(share_opts);
+		right_wrap.append(share_wrap);
+		
+		// create volume menu item
+		vol_btn.text(_getObjectValue(opts.volume));
+		vol_btn.click(function () {});
+		right_wrap.append(vol_btn);
+		
+		// add the righ_wrap to the wrap
+		wrap.append(right_wrap);
+		
+		// finally add the wrap to our container
+		target.empty().append(wrap);
+		
+		// save options
+		target.data('opts', opts);
+		
 		// handle hover
 		if (opacityEnabled) {
 			target.hover(_hoverIn, _hoverOut);
+			_hoverOut.call(target);
 		}
-		
-		// finally add the wrap to our container
-		target.empty().add(wrap);
 	};
 	
 	
